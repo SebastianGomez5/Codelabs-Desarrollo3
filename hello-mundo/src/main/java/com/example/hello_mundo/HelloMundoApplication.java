efc0f93bef3f9f83c4a643e0600d9d5a62b274c8
SebastianGomez5 <juansebastiancali8@gmail.com>
2025-03-27 18:58:05 -0500

    Se añade proyecto inicial

---
+++ hello-mundo/src/main/java/com/example/hello_mundo/HelloMundoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_mundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMundoApplication.class, args);
+	}
+
+}
---
+++ hello-mundo/src/test/java/com/example/hello_mundo/HelloMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_mundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
